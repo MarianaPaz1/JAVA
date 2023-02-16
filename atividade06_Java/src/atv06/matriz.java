1690e4bcc56d5a73e8f449a13d5f24077abf8b1f
MarianaPaz1 <89525684+MarianaPaz1@users.noreply.github.com>
2023-02-16 15:08:59 -0300

    Update matriz.java

--- atividade06_Java/src/atv06/matriz.java
+++ atividade06_Java/src/atv06/matriz.java
@@ -1,30 +1,28 @@
 package atv06;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class matriz {
 public static void main(String[] args) {
 	int[][] valor = new int[3][3];
-	int linha,coluna, x,somaValor=0, res=0;
-	int[] media = new int[10];
+	int linha,coluna, x,somaValor=0;
+	int somaNotaAluno =0, mediaNotaAluno=0,nota, aluno=10;
+	int[] media = new int[aluno];
 	Scanner leia = new Scanner(System.in);
 	
-	for(linha=0;linha<3;linha++) {
-		for(coluna=0;coluna<3;coluna++) {
-			System.out.println("\nEntre com uma nota:");
-			valor[linha][coluna] = leia.nextInt();
-						
-		}
-		for(x=0; x<valor.length; x++) {
-			somaValor += valor[linha][coluna];
-			res=somaValor/4;
-			media[x]++;
-		}
+	for(linha=0;linha<10;linha++) {
+		somaNotaAluno=0;
+		for(coluna=0;coluna<4;coluna++) {
+			System.out.println("\nEntre com a " +(coluna + 1)+" nota do " + (linha+1)+" aluno: ");
+			nota= leia.nextInt();
+			somaNotaAluno += nota;
+			mediaNotaAluno = somaNotaAluno/4;
+			media[linha] = mediaNotaAluno;
 		
-	
-		System.out.println(res);
+				}
 	}
 	
-	}
+	System.out.println(Arrays.toString(media));
 }
-
+}
