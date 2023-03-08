84dfbdcc9b8deb773bd821024e69012c0c9c33c7
Mariana <mnp1807@hotmail.com>
2023-03-08 15:46:08 -0300

    juntando as atividades 08.03

--- BANCO DE DADOS/atividade_02.sql
+++ BANCO DE DADOS/atividade_02.sql
@@ -59,6 +59,44 @@ INSERT INTO tb_personagens (nome, genero, ataque, defesa, categoria_id) values("
 INSERT INTO tb_personagens (nome, genero, ataque, defesa, categoria_id) values("Yago", "masculino", 995,1500,2);
 
 -- ATIVIDADE 02 --
+CREATE DATABASE db_pizzaria_legal;
+USE db_pizzaria_legal;
+-- criando as tabelas --
+CREATE TABLE tb_categoria(
+	id bigint auto_increment,
+    nome varchar(255) not null,
+    tipo varchar(100) not null,
+    primary key (id)
+);
+CREATE TABLE tb_pizza(
+	id bigint auto_increment,
+    nome varchar(255) not null,
+	preco int,
+    tamanho int,
+    borda varchar(255) not null,
+    primary key (id)
+);
+-- criando a chave secundária --
+ALTER TABLE tb_pizza ADD categoria_id bigint; 
+ALTER TABLE tb_pizza ADD CONSTRAINT fk_pizza 
+FOREIGN KEY(categoria_id) REFERENCES tb_categoria(id);
+
+-- Inserindo os tipos de pizza--
+INSERT INTO tb_categoria (nome, tipo) values("Salgada", "Vegana");
+INSERT INTO tb_categoria (nome, tipo) values("Salgada", "Tradicional");
+INSERT INTO tb_categoria (nome, tipo) values("Doce", "Sobremesas");
+INSERT INTO tb_categoria (nome, tipo) values("50/50", "Agridoce");
+INSERT INTO tb_categoria (nome, tipo) values("Doce", "Diet");
+
+-- Inserindo as pizzas--
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Portuguesa", 59 , 8, "recheada", 2);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Carne Seca",59,8, "recheada", 2);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Calabresa",59,8, "recheada", 2);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Mussarela",59,8, "recheada", 2);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Brócolis",68,8, "recheada", 1);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Morango",78,8, "recheada", 3);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Barbecue",108,8, "recheada", 4);
+INSERT INTO tb_pizza (nome, preco, tamanho, borda, categoria_id) values("Coco queimado",34,8, "sem recheio", 5);
 
 -- Verificando as planilhas --
 SELECT * FROM tb_pizza;
